5163698c5a12d15f7d0f59628cef92c0a95f5c3d
Baymax15 <39257699+baymax15@users.noreply.github.com>
2021-11-11 09:28:10 +0530

    HomeScreen hello user

--- app/src/main/java/com/example/registrationform/HomeScreenActivity.java
+++ app/src/main/java/com/example/registrationform/HomeScreenActivity.java
@@ -3,12 +3,31 @@ package com.example.registrationform;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.example.registrationform.Database.AppDatabase;
+import com.example.registrationform.Database.UserDao;
 
 public class HomeScreenActivity extends AppCompatActivity {
+    TextView message;
+    AppDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+
+        db = AppDatabase.getDatabase(HomeScreenActivity.this);
+        UserDao userDao = db.userDao();
+        message = findViewById(R.id.textLogin);
+        int uid = getIntent().getIntExtra("userId", 0);
+        String msg;
+        if (uid != 0) {
+            msg = "Hello, " + userDao.getUser(uid).name;
+        } else {
+            msg = "Uh oh! Something went wrong";
+        }
+        message.setText(msg);
     }
 }
